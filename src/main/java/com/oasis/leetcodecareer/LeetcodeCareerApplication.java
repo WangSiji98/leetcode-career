b56e534e47bbd02556b808ff39e28ea7f2b6b494
WangSiji98 <807403713@qq.com>
2023-10-10 19:03:23 -0700

    Initial commit

---
+++ src/main/java/com/oasis/leetcodecareer/LeetcodeCareerApplication.java
@@ -0,0 +1,13 @@
+package com.oasis.leetcodecareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeCareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeCareerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oasis/leetcodecareer/LeetcodeCareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.leetcodecareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeCareerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
